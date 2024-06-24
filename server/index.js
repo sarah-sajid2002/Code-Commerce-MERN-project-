abc32a82d894663ccee303c5d86e3606e8d445a8
sarah-sajid2002 <sarahsajid19@gmail.com>
2024-06-24 14:27:51 +0500

    connected mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+// importing mongoose
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
+
+// connecting mongoose
+mongoose
+  .connect("mongodb://127.0.0.1/CodeCommerce")
+  .then((res) => console.log("connected to mongodb..." + res))
+  .catch((err) => console.error("could not connect to mongodb " + err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

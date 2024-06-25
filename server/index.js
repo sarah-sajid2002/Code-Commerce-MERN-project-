eba5d126d15bbad719faecc0a0ae25b7b2edefbe
sarah-sajid2002 <sarahsajid19@gmail.com>
2024-06-25 17:45:23 +0500

    added Product Model, Post Route

--- server/index.js
+++ server/index.js
@@ -1,17 +1,41 @@
 const express = require("express");
-// importing mongoose
 const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
+const Product = require("./models/product");
 
-// connecting mongoose
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
 mongoose
-  .connect("mongodb://127.0.0.1/CodeCommerce")
+  .connect(
+    // "mongodb+srv://sarahsajid19:3QcEHlfEcJNYZplC@cluster0.udd7a6x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    "mongodb://127.0.0.1/CodeCommerce"
+  )
   .then((res) => console.log("connected to mongodb..." + res))
   .catch((err) => console.error("could not connect to mongodb " + err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/create", async (req, res) => {
+  try {
+    const product = new Product({ ...req.body });
+    await product.save();
+    res.status(201).json({
+      status: "Success",
+      message: "Product saved to the database!",
+      product,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Some thing went wrong",
+    });
+  }
 });
 
 app.listen(port, () => {

74f85d4e2bf5072737e6d3b2dc9bd29b25e9420d
sarah-sajid2002 <sarahsajid19@gmail.com>
2024-06-27 14:53:47 +0500

    completed the crud operations

--- server/index.js
+++ server/index.js
@@ -38,6 +38,50 @@ app.post("/create", async (req, res) => {
   }
 });
 
+//Get the all product list
+app.get("/read", async (req, res) => {
+  const productList = await Product.find({});
+  try {
+    res.status(200).json({
+      status: "Success",
+      data: {
+        productList,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed",
+      message: err,
+    });
+  }
+});
+
+//Update a product based on the id
+app.put("/update/:id", async (req, res) => {
+  const product_id = req.params.id;
+  await Product.findByIdAndUpdate(product_id, {
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    discountPercentage: req.body.discountPercentage,
+    rating: req.body.rating,
+    stock: req.body.stock,
+    brand: req.body.brand,
+    category: req.body.category,
+    thumbnail: req.body.thumbnail,
+    images: req.body.images,
+  });
+
+  res.send("Product updated successfully!");
+});
+
+//Delete a product based on the id
+app.delete("/delete/:id", async (req, res) => {
+  const product_id = req.params.id;
+  await Product.findByIdAndDelete(product_id);
+  res.send("Product deleted!");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
